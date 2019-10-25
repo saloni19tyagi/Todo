d248e735ed9614c827dc68b2a28088c234940e73
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-10-25 15:14:41 +0530

    Dynamic Styling

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ function addItem() {
 		var node = document.createElement("li") ;
 		var btn = document.createElement("button") ;
 		btn.innerText = "Remove" ;
+		
+		btn.setAttribute("style","border-radius:10px","border:none","padding:0.4rem") ;
 		node.appendChild(item);
 		node.appendChild(btn) ;
 		
+
 		btn.addEventListener("click",function() {
 			this.parentNode.remove() ;
 		})
 		
+		// node.setAttribute("style",)
 		document.getElementById("itemlist").appendChild(node) ;  
 		document.getElementById("item").value="" ;
 	}
